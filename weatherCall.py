4c8f59e9987381e2e1376299a138d2438cba6951
teemuhirvonen <Teemuh25@gmail.com>
2019-12-03 23:17:22 +0200

    final changes

--- weatherCall.py
+++ weatherCall.py
@@ -1,20 +1,17 @@
-# Get api id from https://openweathermap.org/api
-# Mail credentials from https://mailtrap.io/
+# Get api id from https://openweathermap.org/api, replace YourApiId
+# Login credentials from https://mailtrap.io/
 
 import smtplib
 import datetime
 import smtplib
 import requests
 
-temperatureArr = []
-weatherArr = []
-
 # Initial api call
 api_address = 'http://api.openweathermap.org/data/2.5/forecast?appid=YourApiId&units=metric&q='
 city = input('Input city name: ')
 
 url = api_address + city
-json_data = requests.get(api_address).json()
+json_data = requests.get(url).json()
 
 # Initial date time get
 date_array = []
@@ -27,30 +24,34 @@ for i in range(6):
     date_array.append(formatted_date)
 
 # Fetching wanted information from api
-for i in range(39):
+temperatureArr = []
+weatherArr = []
+for i in range(38):
     somestring = json_data['list'][i]['dt_txt']
     if "12:00:00" in somestring:
         temperatureArr.append(json_data['list'][i]['main']['temp'])
         weatherArr.append(json_data['list'][i]['weather'][0]['description'])
 
 # Information to be send via email
+msg = ""
 for i in range(5):
-    day = 'Date: ' + date_array[i] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[i]) + \
-       ", " + weatherArr[i]
-    msg = msg + day + "\n"
+    msg += 'Date: ' + date_array[i] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[i]) + \
+       ", " + weatherArr[i] + "\n"
 
 # Email Information
 sender = "Mikko Mallikas"
 receiver = "Teemu Hirvonen"
 
 message = f"""\
-Subject: Weather Forecast
+Subject: Weather forecast
 To: {receiver}
 From: {sender}
 
 {msg}"""
 
-# Sending email
+# Sending email, replace Login information here
 with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
     server.login("LoginUser", "LoginPass")
     server.sendmail(sender, receiver, message)
+
+print("Email sent")
--- weatherWeekly.py
+++ weatherWeekly.py
@@ -1,69 +1,66 @@
+# Get api id from https://openweathermap.org/api, replace YourApiId
+# Login credentials from https://mailtrap.io/
+
+# Still not working right, not sending mail when sheduled time comes
+
 import smtplib
 import datetime
 import smtplib
 import requests
 import schedule
 import time
+from time import sleep
 
 def callWeather():
-    temperatureArr = []
-    weatherArr = []
 
-    # API haku
-    api_address = 'http://api.openweathermap.org/data/2.5/forecast?appid=APPID&units=metric&q=Helsinki'
+    # Initial api call
+    api_address = 'http://api.openweathermap.org/data/2.5/forecast?appid=YourApiId&units=metric&q=Helsinki'
     json_data = requests.get(api_address).json()
 
-    # Päivä tiedot
+    # Initial date time get
     date_array = []
     today = datetime.date.today()
 
-    # Päivämäärän asetus
+    # Setting dates to array
     for i in range(6):
         date = today + datetime.timedelta(days=i)
         formatted_date = date.strftime('%d.%m.%Y')
         date_array.append(formatted_date)
 
-    # Tietojen haku APIsta
+    # Fetching wanted information from api
+    temperatureArr = []
+    weatherArr = []
     for i in range(39):
         somestring = json_data['list'][i]['dt_txt']
         if "12:00:00" in somestring:
             temperatureArr.append(json_data['list'][i]['main']['temp'])
             weatherArr.append(json_data['list'][i]['weather'][0]['description'])
 
-    # Lähetettävät tiedot
-    day1 = 'Date: ' + date_array[0] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[0]) + \
-        ", " + weatherArr[0]
+    # Information to be send via email
+    msg = ""
+    for i in range(5):
+        msg += 'Date: ' + date_array[i] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[i]) + \
+            ", " + weatherArr[i] + "\n"
 
-    day2 = 'Date: ' + date_array[0] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[1]) + \
-        ", " + weatherArr[1]
-
-    day3 = 'Date: ' + date_array[0] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[2]) + \
-        ", " + weatherArr[2]
-
-    day4 = 'Date: ' + date_array[0] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[3]) + \
-        ", " + weatherArr[3]
-
-    day5 = 'Date: ' + date_array[0] + ' 12:00, ' + 'Temperature: ' + str(temperatureArr[4]) + \
-        ", " + weatherArr[4]
-    msg = day1 + '\n' + day2 + '\n' + day3 + '\n' + day4 + '\n' + day5
-
-    # Sähköpostin tiedot
+    # Email Information
     sender = "Mikko Mallikas"
     receiver = "Teemu Hirvonen"
+
     message = f"""\
     Subject: Weather Forecast
     To: {receiver}
     From: {sender}
+
     {msg}"""
 
-    # Sähköpostin lähetys
+    # Sending email, replace Login information here
     with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
         server.login("LoginUser", "LoginPass")
         server.sendmail(sender, receiver, message)
 
     print('Email sent')
 
-
+# Scheduled call to callWeather function (not properly working)
 schedule.every().monday.at("08:00").do(callWeather)
 
 while True:
